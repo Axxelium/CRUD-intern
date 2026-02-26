3ba4731561e82f83ba4afa7865648b16e6b087bb
Axxelium <59525687+Axxelium@users.noreply.github.com>
2026-02-27 02:04:52 +0500

    Create a Spring Boot project
    Information about build:
    Build tool - Maven
    Lang - Java 25
    Spring Boot version - 4.0.3
    
    Dependencies - Spring Web, PostrgeSQL Driver
    
    Structure of project generated at https://start.spring.io/

---
+++ CRUD-intern/src/main/java/com/arnurproject/CRUD_intern/CrudInternApplication.java
@@ -0,0 +1,13 @@
+package com.arnurproject.CRUD_intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudInternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudInternApplication.class, args);
+	}
+
+}
---
+++ CRUD-intern/src/test/java/com/arnurproject/CRUD_intern/CrudInternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnurproject.CRUD_intern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudInternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
